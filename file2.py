236d2c0d3241612631bd555b69b66fde7b85ad86
irkghub <bkbhandari99@gmail.com>
2024-10-16 21:57:41 +0545

    Another mini project added

---
+++ file2.py
@@ -0,0 +1,6 @@
+print("Welcome fellas to this Band Name Generator Game !!!!")
+print("Before that we'd like to know a few things about ya!")
+city = input("Which city were you born ?\n")
+pet_name = input("I guess you did have a pet, didn't you ?\n What did you use to call it ?\n")
+
+print(f"Boom!!! Bang!!!\n{city}{pet_name} is here folks\nWhen\'s your next show ?")
